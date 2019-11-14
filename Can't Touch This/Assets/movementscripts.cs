e5eaf6e801b01abf98ce6f952c5c234ec0cf6520
Camilla-Z <congying.camilla@gmail.com>
2019-11-13 21:16:46 -0500

    Camilla11.13

---
+++ Can't Touch This/Assets/movementscripts.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(Animator))]
+public class movementscripts : MonoBehaviour
+{
+    [SerializeField]
+    Animator am;
+    [SerializeField]
+    SpriteRenderer sr;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            am.SetTrigger("jump")
+        }
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            am.SetTrigger("Down");
+        }
+            
+        transform.position += new Vector3(
+            Input.GetAxis("Horizontal"),0, 0)
+            * Time.deltaTime * 10;
+        am.SetFloat("Speed",Input.GetAxis("Horizontal"));
+        //if (Input.GetAxis("Horizontal") < 0)
+        //    sr.flipX = true;
+        //else if (Input.GetAxis("Horizontal") > 0)
+        //    sr.flipX = false;
+
+    }
+}
