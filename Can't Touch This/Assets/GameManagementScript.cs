c6bbf04a2f95d9eab7b937a94f5462a855eeb157
L C <llchen@andrew.cmu.edu>
2019-11-27 02:57:43 -0500

    Minor Menu updates (In game menu added, not tested)

---
+++ Can't Touch This/Assets/GameManagementScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManagementScript : MonoBehaviour
+{
+    public void pauseGame()
+    {
+        Time.timeScale = 0;
+    }
+    public void unPauseGame()
+    {
+        Time.timeScale = 1;
+    }
+    public void quitToMain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+}
